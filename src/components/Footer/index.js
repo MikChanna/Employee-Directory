0f3e2896810eef7289d0abc6b5276343590c45a7
MikChanna <channa.mik@gmail.com>
2020-10-06 19:59:27 -0400

    added footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "/style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>Employee Directory App 2020 CLMC</span>
+    </footer>
+  );
+}
+
+export default Footer;
