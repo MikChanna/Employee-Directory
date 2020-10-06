c238fe976754e75a751b381abf08de6d1d3312d8
MikChanna <channa.mik@gmail.com>
2020-10-06 19:24:24 -0400

    imported pages to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./pages/Header";
+import Search from "./pages/Search";
+import Results from "./pages/Results";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Search />
+      <Results />
     </div>
   );
 }
