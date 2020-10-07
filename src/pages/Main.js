2b3c3d12d3ba4bcd902bf45a63cc6b5f392c33f9
MikChanna <channa.mik@gmail.com>
2020-10-06 21:27:55 -0400

    redid main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,21 +1,18 @@
 import React from "react";
-import Container from "../components/Container";
+// import Container from "../components/Container";
 import Button from "../components/Button";
 import Navbar from "../components/Navbar";
 import Searchbar from "../components/Searchbar";
-import Searchresults from "../components/Searchresults";
+// import Searchresults from "../components/Searchresults";
 import Footer from "../components/Footer";
 
 function Main() {
   return (
     <div>
-      <Container>
-        <Navbar />
-        <Searchbar />
-        <Button />
-        <Searchresults />
-        <Footer />
-      </Container>
+      <Navbar />
+      <Searchbar />
+      <Button />
+      <Footer />
     </div>
   );
 }
