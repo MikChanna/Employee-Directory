41e55f686a26614fe7e002dc7205b871d36065d1
MikChanna <channa.mik@gmail.com>
2020-10-11 14:11:21 -0400

    added to searchbar

--- src/components/Searchbar/index.js
+++ src/components/Searchbar/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+
 function Searchbar() {
-  return <input type="text" className="searchbar" id="searchInput"></input>;
+  
+  return <div className = "row">
+    <input type="text" className="searchbar" id="searchInput"></input>
+    <button className="column" id="submitSearch">
+      Submit
+    </button>
+    </div>
+  
+
 }
 
 export default Searchbar;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,19 +1,20 @@
 import React from "react";
 // import Container from "../components/Container";
-import Button from "../components/Button";
+// import Button from "../components/Button";
 import Navbar from "../components/Navbar";
 import Searchbar from "../components/Searchbar";
+// import Row from "../components/Row";
 // import Searchresults from "../components/Searchresults";
 import Footer from "../components/Footer";
 
 function Main() {
   return (
-    <div>
+  <div>
       <Navbar />
-      <Searchbar />
-      <Button />
-      <Footer />
-    </div>
+        <Searchbar />
+        <Footer />
+  </div>
+  
   );
 }
 
